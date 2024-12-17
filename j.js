a22b17adea3fe74ad22372dd968d3136c94c6188
Mohamed Farag <medocena1997@gmail.com>
2024-12-17 23:07:41 +0200

    Update j.js

--- j.js
+++ j.js
@@ -1,11 +1,11 @@
-alert("user names : hassan or farag or let it empty")
+alert("user names : cody or roman or let it empty")
 let hassan = prompt("guess what is in the mystery box : ")
 let btn=getElementById("btn");
 function tamerHosny(btn){
-  if (hassan === "hassan"){
+  if (hassan === "cody"){
     document.getElementById("easy").innerHTML="كسم كودي رودز";
-  }else if(hassan==="farag"){
-    document.getElementById("easy").innerHTML="كسمين ام كودي رودز ";
+  }else if(hassan==="roman"){
+    document.getElementById("easy").innerHTML="كسمين ام كودي رودز و اعترف بالزعيم ";
   }else{
       document.getElementById("easy").innerHTML=" انت عشمان في ايه؟ كسم كودي رودز ";
   };
