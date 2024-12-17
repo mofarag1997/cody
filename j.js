833febc0cfded1847079b222ebdb58134376d27a
Mohamed Farag <medocena1997@gmail.com>
2024-12-17 23:05:56 +0200

    Update j.js

--- j.js
+++ j.js
@@ -3,23 +3,12 @@ let hassan = prompt("guess what is in the mystery box : ")
 let btn=getElementById("btn");
 function tamerHosny(btn){
   if (hassan === "hassan"){
-    document.getElementById("easy").innerHTML="كسم تامر حسني";
+    document.getElementById("easy").innerHTML="كسم كودي رودز";
   }else if(hassan==="farag"){
-    document.getElementById("easy").innerHTML="كسم حسن فرج ";
+    document.getElementById("easy").innerHTML="كسمين ام كودي رودز ";
   }else{
-      document.getElementById("easy").innerHTML=" كسم حسن فرج برضوا ";
-  };let job = "Manager";
-  let salary = 0;
-  
-  if (job === "Manager") {
-    salary = 8000;
-  } else if (job === "IT" || job === "Support") {
-    salary = 6000;
-  } else if (job === "Developer" || job === "Designer") {
-    salary = 7000;
-  } else {
-    salary = 4000;
-  }
+      document.getElementById("easy").innerHTML=" انت عشمان في ايه؟ كسم كودي رودز ";
+  };
   
   btn.hidden=true;
 };
